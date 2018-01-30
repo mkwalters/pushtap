1668facb5531e0dc81e1516726bc273abc65df72
Mitchell Walters <mitchellwalters@Mitchells-MacBook-Pro.local>
2018-01-30 09:28:02 -0800

    i just put in all of the possible things gad delegate could listen for

--- PushTap/GameViewController.swift
+++ PushTap/GameViewController.swift
@@ -43,17 +43,35 @@ class GameViewController: UIViewController, GADInterstitialDelegate {
         
     }
     
-    func interstitialDidDismissScreen(_ ad: GADInterstitial) {
-        //interstitial = createAndLoadInterstitial()
-        
-        print("i just dismissed the interstitial ad")
-        
+    /// Tells the delegate an ad request succeeded.
+    func interstitialDidReceiveAd(_ ad: GADInterstitial) {
+        print("interstitialDidReceiveAd")
     }
     
-    func interstitialDidReceiveAd(_ ad: GADInterstitial) {
-        
-        print("interstitialDidReceiveAd")
-        
+    /// Tells the delegate an ad request failed.
+    func interstitial(_ ad: GADInterstitial, didFailToReceiveAdWithError error: GADRequestError) {
+        print("interstitial:didFailToReceiveAdWithError: \(error.localizedDescription)")
+    }
+    
+    /// Tells the delegate that an interstitial will be presented.
+    func interstitialWillPresentScreen(_ ad: GADInterstitial) {
+        print("interstitialWillPresentScreen")
+    }
+    
+    /// Tells the delegate the interstitial is to be animated off the screen.
+    func interstitialWillDismissScreen(_ ad: GADInterstitial) {
+        print("interstitialWillDismissScreen")
+    }
+    
+    /// Tells the delegate the interstitial had been animated off the screen.
+    func interstitialDidDismissScreen(_ ad: GADInterstitial) {
+        print("interstitialDidDismissScreen")
+    }
+    
+    /// Tells the delegate that a user click will open another app
+    /// (such as the App Store), backgrounding the current app.
+    func interstitialWillLeaveApplication(_ ad: GADInterstitial) {
+        print("interstitialWillLeaveApplication")
     }
     
     @IBAction func showAd(_ sender: AnyObject) {
